bdbd5af1fd94e919b37ebba0435015e6abab475d
samdouble <samleafs@protonmail.com>
2025-12-28 17:33:04 -0500

    feat(mysql): added connector

---
+++ src/MysqlConnector.ts
@@ -0,0 +1,64 @@
+import { Knex } from "knex";
+import { IConnector } from "./IConnector";
+import { Column, Ddl, MysqlColumn, MysqlTable, Table } from "./types";
+
+export class MysqlConnector implements IConnector {
+  constructor(private readonly db: Knex) {
+    this.db = db;
+  }
+
+  async getColumns(tableName: string): Promise<Column[]> {
+    const result = await this.db.raw(`
+      select * from information_schema.columns 
+      where
+        table_name=?
+        and table_schema=database()
+      `,
+      [tableName],
+    );
+    return result[0].map((c: MysqlColumn) => ({
+      cid: c['ORDINAL_POSITION'],
+      name: c['COLUMN_NAME'],
+      type: c['COLUMN_TYPE'],
+      notnull: c['IS_NULLABLE'] === 'NO',
+      dflt_value: c['COLUMN_DEFAULT'],
+      pk: c['COLUMN_KEY'] === 'PRI',
+    }));
+  }
+
+  async getDdl(tableName: string): Promise<Ddl | null> {
+    const exists = await this.tableExists(tableName);
+    if (!exists) {
+      return null;
+    }
+    const result = await this.db.raw(`
+      show create table ${tableName}
+    `);
+    return result[0][0]['Create Table'];
+  }
+
+  async getTables(): Promise<Table[]> {
+    const result = await this.db.raw(`
+      select * from information_schema.tables
+      where
+        table_type = 'BASE TABLE'
+        and table_schema = database()
+        and table_name not like 'knex_%'
+    `);
+    return result[0].map((t: MysqlTable) => ({
+      name: t['TABLE_NAME'],
+      schema: t['TABLE_SCHEMA'],
+      database: t['TABLE_CATALOG'],
+    }));
+  }
+
+  async tableExists(tableName: string): Promise<boolean> {
+    const result = await this.db.raw(`
+      select count(*) as cnt from information_schema.tables
+      where
+        table_name = ?
+        and table_schema = database()
+    `, [tableName]);
+    return result[0][0]['cnt'] > 0;
+  }
+}
--- src/types.ts
+++ src/types.ts
@@ -7,10 +7,30 @@ export type Column = {
   pk: boolean;
 };
 
-export type Ddl = {
-  sql: string;
+export type Ddl = string;
+
+export type MysqlTable = {
+  TABLE_NAME: string;
+  TABLE_SCHEMA: string;
+  TABLE_CATALOG: string;
+};
+
+export type MysqlColumn = {
+  COLUMN_DEFAULT: string;
+  COLUMN_KEY: string;
+  COLUMN_NAME: string;
+  COLUMN_TYPE: string;
+  IS_NULLABLE: string;
+  ORDINAL_POSITION: number;
+};
+export type SqliteTable = {
+  name: string;
+  schema: string;
+  database: string;
 };
 
 export type Table = {
   name: string;
+  schema: string;
+  database: string;
 };

58a4e658c1296fb23fce6fd716879cbc67fc57af
samdouble <samleafs@protonmail.com>
2025-12-28 22:21:03 -0500

    ci: set up Release-Please for versioning

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import { MermaidVisualizer } from './visualizers/MermaidVisualizer';
-
 export { IConnector } from './connectors/IConnector';
 export { MysqlConnector } from './connectors/MysqlConnector';
 export { SqliteConnector } from './connectors/SqliteConnector';
@@ -7,6 +6,8 @@ export { Column, Ddl, Table } from './connectors/types';
 export { MermaidVisualizer } from './visualizers/MermaidVisualizer';
 
 export const visualize = async (_output: string) => {
+  // const knexfilePath = path.join(process.cwd(), 'knexfile.ts');
+  // console.log(knexfilePath);
   const visualizer = new MermaidVisualizer();
   return visualizer.visualize([], []);
 };

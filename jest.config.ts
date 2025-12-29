e1d9312cb73cc14e1c7233058deb9ca1144ef0f8
samdouble <samleafs@protonmail.com>
2025-12-29 11:35:36 -0500

    test: measure test coverage

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,17 @@
 export default {
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{ts,tsx}'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  coverageReporters: ['json', 'lcov', 'text', 'clover'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 60,
+      lines: 50,
+      statements: 50,
+    },
+  },
   preset: 'ts-jest',
   testEnvironment: 'node',
 };

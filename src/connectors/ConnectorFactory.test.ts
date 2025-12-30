580ea74973255060de8a846f08cb53b0a075ee99
samdouble <samleafs@protonmail.com>
2025-12-30 11:20:55 -0500

    feat(postgresql): added connector

--- src/connectors/ConnectorFactory.test.ts
+++ src/connectors/ConnectorFactory.test.ts
@@ -1,11 +1,17 @@
 import { ConnectorFactory } from "./ConnectorFactory";
 import { MysqlConnector } from "./MysqlConnector";
+import { PostgresConnector } from "./PostgresConnector";
 import { SqliteConnector } from "./SqliteConnector";
 
 describe('ConnectorFactory', () => {
   it('should create a MySQL connector', () => {
     const connector = ConnectorFactory.create('mysql');
     expect(connector).toBeInstanceOf(MysqlConnector);
+  });
+
+  it('should create a PostgreSQL connector', () => {
+    const connector = ConnectorFactory.create('pg');
+    expect(connector).toBeInstanceOf(PostgresConnector);
   });
 
   it('should create a SQLite connector', () => {
